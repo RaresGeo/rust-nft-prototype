f0f5888eaca5a2df54dc8d4a0e76cf7433e1522f
Daniel WSL PC <raresgeo@gmail.com>
2022-01-31 17:09:38 +0200

    Added unit tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::collections::{LazyOption, LookupMap, UnorderedMap, UnorderedSet};
 use near_sdk::json_types::{Base64VecU8, U128};
@@ -6,23 +5,24 @@ use near_sdk::serde::{Deserialize, Serialize};
 use near_sdk::{
     env, near_bindgen, AccountId, Balance, CryptoHash, PanicOnDefault, Promise, PromiseOrValue,
 };
+use std::collections::HashMap;
 
+pub use crate::approval::*;
+pub use crate::events::*;
 use crate::internal::*;
 pub use crate::metadata::*;
 pub use crate::mint::*;
 pub use crate::nft_core::*;
-pub use crate::approval::*;
 pub use crate::royalty::*;
-pub use crate::events::*;
 
-mod internal;
-mod approval; 
-mod enumeration; 
-mod metadata; 
-mod mint; 
-mod nft_core; 
-mod royalty; 
+mod approval;
+mod enumeration;
 mod events;
+mod internal;
+mod metadata;
+mod mint;
+mod nft_core;
+mod royalty;
 
 /// This spec can be treated like a version of the standard.
 pub const NFT_METADATA_SPEC: &str = "nft-1.0.0";
@@ -70,7 +70,7 @@ impl Contract {
     */
     #[init]
     pub fn new_default_meta(owner_id: AccountId) -> Self {
-        //calls the other function "new: with some default metadata and the owner_id passed in 
+        //calls the other function "new: with some default metadata and the owner_id passed in
         Self::new(
             owner_id,
             NFTContractMetadata {
@@ -88,11 +88,11 @@ impl Contract {
     /*
         initialization function (can only be called once).
         this initializes the contract with metadata that was passed in and
-        the owner_id. 
+        the owner_id.
     */
     #[init]
     pub fn new(owner_id: AccountId, metadata: NFTContractMetadata) -> Self {
-        //create a variable of type Self with all the fields initialized. 
+        //create a variable of type Self with all the fields initialized.
         let this = Self {
             //Storage keys are simply the prefixes used for the collections. This helps avoid data collision
             tokens_per_owner: LookupMap::new(StorageKey::TokensPerOwner.try_to_vec().unwrap()),
@@ -100,7 +100,7 @@ impl Contract {
             token_metadata_by_id: UnorderedMap::new(
                 StorageKey::TokenMetadataById.try_to_vec().unwrap(),
             ),
-            //set the owner_id field equal to the passed in owner_id. 
+            //set the owner_id field equal to the passed in owner_id.
             owner_id,
             metadata: LazyOption::new(
                 StorageKey::NFTContractMetadata.try_to_vec().unwrap(),
@@ -110,5 +110,84 @@ impl Contract {
 
         //return the Contract object
         this
+    }
+}
+
+use crate::nft_core::NonFungibleTokenCore;
+
+#[cfg(all(test, not(target_arch = "wasm32")))]
+mod tests {
+    use near_sdk::test_utils::{accounts, VMContextBuilder};
+    use near_sdk::testing_env;
+
+    use super::*;
+
+    const MINT_STORAGE_COST: u128 = 90000000000000000000000;
+
+    fn get_context(predecessor_account_id: AccountId) -> VMContextBuilder {
+        let mut builder = VMContextBuilder::new();
+        builder
+            .current_account_id(accounts(0))
+            .signer_account_id(predecessor_account_id.clone())
+            .predecessor_account_id(predecessor_account_id);
+        builder
+    }
+
+    #[test]
+    fn test_new() {
+        let mut context = get_context(accounts(1));
+        testing_env!(context.build());
+        let contract = Contract::new_default_meta(accounts(1).into());
+        testing_env!(context.is_view(true).build());
+        assert_eq!(contract.nft_tokens(None, Some(10)).len(), 0);
+    }
+
+    #[test]
+    #[should_panic(expected = "The contract is not initialized")]
+    fn test_default() {
+        let context = get_context(accounts(1));
+        testing_env!(context.build());
+        let _contract = Contract::default();
+    }
+
+    // This value might change in the future and so this test could fail.
+    // I could fix this by changing the panic message in nft_mint but then it wouldn't be as useful for its actual purpose
+    #[test]
+    #[should_panic(expected = "Must attach 6630000000000000000000 yoctoNEAR to cover storage")]
+    fn test_no_deposit() {
+        let mut context = get_context(accounts(0));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default_meta(accounts(0).into());
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .predecessor_account_id(accounts(0))
+            .build());
+
+        let token_id = "0".to_string();
+        contract.nft_mint(token_id.clone(), accounts(0), None);
+    }
+
+    #[test]
+    fn test_mint() {
+        let mut context = get_context(accounts(0));
+        testing_env!(context.build());
+        let mut contract = Contract::new_default_meta(accounts(0).into());
+
+        testing_env!(context
+            .storage_usage(env::storage_usage())
+            .attached_deposit(MINT_STORAGE_COST)
+            .predecessor_account_id(accounts(0))
+            .build());
+
+        let token_id = "0".to_string();
+        contract.nft_mint(token_id.clone(), accounts(0), None);
+        let token = contract.nft_token(token_id).unwrap();
+        assert_eq!(token.token_id.to_string(), "0".to_string());
+        assert_eq!(token.owner_id.to_string(), accounts(0).to_string());
+        println!(
+            "\n\n NEWLY MINTED TOKEN:\n\nToken id: {}, owner id: {}\n\n",
+            token.token_id, token.owner_id
+        );
     }
 }
